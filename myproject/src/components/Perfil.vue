174982bb88a7267aa6187373c0a6466776150f28
michelecruz <michelecruz.sb@gmail.com>
2019-04-11 13:01:37 -0300

    tela perfil

---
+++ myproject/src/components/Perfil.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar-brand href="#"></b-navbar-brand>
+
+    <b-navbar-brand href="#"><b>UCAN</b></b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-navbar-brand href="#"></b-navbar-brand>
+        <b-navbar-brand href="#"></b-navbar-brand>
+        <b-navbar-brand href="#">Sair</b-navbar-brand>
+
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+<b-container class="bv-example-row">
+  <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+  <b-row>
+    <b-col cols="12" md="4">
+        <div class="mt-3">
+  <b-card
+    border-variant="primary"
+    no-body
+    style="max-width: 20rem;"
+    img-src="https://1cfrbv1cz8j13t7nr4n67ss1-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/CX-Superhero-preview.png"
+    img-alt="Image"
+    img-top
+  >
+    <h4 slot="header">Nome</h4>
+
+    <b-card-body>
+        <!-- <b-list-group flush> -->
+        <b-list-group-item>Faculdade</b-list-group-item>
+        <b-list-group-item>Curso</b-list-group-item>
+        <b-list-group-item>Matrícula</b-list-group-item>
+        <!-- <b-list-group-item>Vestibulum at eros</b-list-group-item> -->
+    <!-- </b-list-group> -->
+      <!-- <b-card-title>Card Title</b-card-title>
+      <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title> -->
+      <!-- <b-card-text>
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </b-card-text> -->
+    </b-card-body>
+    <!-- <b-card-body>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </b-card-body> -->
+  </b-card>
+</div>
+    </b-col>
+    <b-col cols="12" md="8">
+        <div class="mt-3">
+    <b-card-group deck>
+        <b-card border-variant="primary" header="Personalidade" align="center">
+        <!-- <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text> -->
+      </b-card>
+      <b-card border-variant="primary" header="Conquistas" align="center">
+        <!-- <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text> -->
+      </b-card>
+    </b-card-group>
+  </div>
+  <div class="mt-3">
+    <b-card-group deck>
+      <b-card border-variant="primary" header="Profissões" align="center">
+        <h6>Profissão A</h6>
+        <div><b-progress :value="value" class="mb-3"></b-progress></div>
+      </b-card>
+    </b-card-group>
+  </div>
+  <div class="mt-3">
+    <b-card-group deck>
+      <b-card border-variant="primary" header="Materiais" align="center">
+         <h6>Material A</h6>
+        <div><b-progress :value="value" class="mb-3"></b-progress></div>
+      </b-card>
+    </b-card-group>
+  </div>
+</b-col>
+</b-row>
+</b-container>
+  </div>
+</template>
--- myproject/src/router/index.js
+++ myproject/src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Perfil from '@/components/Perfil'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import Layout from 'bootstrap-vue/es/components/layout'
+import Progress from 'bootstrap-vue/es/components/progress'
+Vue.use(Progress)
+Vue.use(Layout)
 Vue.use(BootstrapVue)
 Vue.use(Router)
 
@@ -14,6 +18,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/perfil',
+      name: 'Perfil',
+      component: Perfil
     }
   ]
 })
